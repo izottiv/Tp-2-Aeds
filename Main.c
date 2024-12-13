4f3b235218746f79df65d7665008078ee27eaa6f
Vitor Carvalho <vitorafonso20040@gmail.com>
2024-12-12 21:33:09 -0300

    agora sim

--- Main.c
+++ Main.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 #include <time.h>
 #include "CentralDeComando/TAD_CentraDeComando.h"
 #include "Arquivo/EntradaDeArquivo.h"
 
 
 int main() {
-    int escolha;
-    printf("Deseja realizar a entrada de dados por (1 - Arquivo 2 - Terminal): ");
+    int escolha, escolhaarquivo;
+    printf("Deseja realizar a entrada de dados por (1- Arquivo 2- Terminal): ");
     scanf("%d", &escolha);
+    char nome[50];
     if(escolha == 1){
-    char nome[] = {"Arquivo/teste.txt"};
+        printf("Deseja utilizar um arquivo com quantas rochas ?\n 1- 10 Rochas\n 2- 15 Rochas\n 3- 20 Rochas\n 4- 25 Rochas\n 5- 50 Rochas\n");
+        printf("Escolha: ");
+        scanf("%d", &escolhaarquivo);
+        switch (escolhaarquivo)
+        {
+        case 1:
+            strcpy(nome,"Arquivo/10Rochas.txt");
+            break;
+        case 2:
+            strcpy(nome,"Arquivo/15Rochas.txt");
+            break;
+        case 3:
+            strcpy(nome,"Arquivo/20Rochas.txt");
+            break;
+        case 4:
+            strcpy(nome,"Arquivo/25Rochas.txt");
+            break;
+        case 5:
+            strcpy(nome,"Arquivo/50Rochas.txt");
+            break;
+        default:
+            break;
+        }
     clock_t Ticks[2];
     Ticks[0] = clock();
     arquivo(nome);
